b115fcea4b7ef81d8329ce06b38fefdff072cf53
Alexander Kotov <amikotov@gmail.com>
2016-07-21 16:05:16 +0300

    [WIP] Add e-mail sending

--- index.rb
+++ index.rb
@@ -13,7 +13,7 @@ post '/' do
     :address        => ENV['MAILGUN_SMTP_PORT'],
     :port           => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],,
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain         => 'arcane-plateau-47565.heroku.com' # the HELO domain provided by the client to the server
   }
