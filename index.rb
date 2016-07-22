4fb84ef7e4e5ddb27645d36f9ddfa7a6d7020b51
Alexander Kotov <amikotov@gmail.com>
2016-07-22 08:02:58 +0300

    Add email send, fix some bugs

--- index.rb
+++ index.rb
@@ -1,24 +1,33 @@
 require 'sinatra'
+require 'pony'
+
+
+Pony.options= {
+  :from => 'from-you@portfolio.com',
+  :via => :smtp,
+  :via_options => {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => ' heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+    }
+  }
 
 get '/' do
   erb :index
 end
 
 post '/' do
-  require 'pony'
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
   Pony.mail({
-  :to => 'amikotov@gmail.com',
-  :header => 'Test',
-  :body => 'Test',
-  :via => :smtp,
-  :via_options => {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :domain => 'heroku.com',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  :to => 'oliktva@gmail.com',
+  :subject => "New question from #{@name}, #{@email}",
+  :body => "#{@message}"
 })
+redirect to('/')
 end
